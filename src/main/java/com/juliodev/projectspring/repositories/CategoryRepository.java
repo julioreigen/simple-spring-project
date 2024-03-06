0b87ed3759d19e4067ebfb2cd7f55423d4cea652
Julio de Brito <juliobrito.tec@gmail.com>
2024-03-06 18:41:35 -0300

    feat: add H2 Database

--- src/main/java/com/juliodev/projectspring/repositories/CategoryRepository.java
+++ src/main/java/com/juliodev/projectspring/repositories/CategoryRepository.java
@@ -1,27 +1,10 @@
 package com.juliodev.projectspring.repositories;
 
 import com.juliodev.projectspring.entities.Category;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-@Component
-public class CategoryRepository {
-
-    private final Map<Long, Category> map = new HashMap<>();
-
-    public void save(Category category) {
-        map.put(category.getId(), category);
-    }
-
-    public Category findById(Long id) {
-        return map.get(id);
-    }
-
-    public List<Category> findAll() {
-        return new ArrayList<Category>(map.values());
-    }
 }
--- src/main/java/com/juliodev/projectspring/resources/CategoryResource.java
+++ src/main/java/com/juliodev/projectspring/resources/CategoryResource.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -28,7 +27,7 @@ public class CategoryResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Category> findById(@PathVariable Long id) {
-        Category cat = categoryRepository.findById(id);
+        Category cat = categoryRepository.findById(id).get();
 
         return ResponseEntity.ok().body(cat);
     }
